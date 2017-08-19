5564aa2e23c901ebedf04201ae42f8e395cb0cdd
Julien BONACHERA <jbonachera@xebia.fr>
2017-08-19 22:55:03 +0200

    censor username and password

--- src/components/MQTTServerStatus.vue
+++ src/components/MQTTServerStatus.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="devices row center-block">
     <div class="col-md-12">
-      <span v-if="mqtt_status === 'ONLINE'">Connected to: {{ mqtt_server }}</span>
-      <span v-else>Disconnected from: {{ mqtt_server }}</span>
+      <span v-if="mqtt_status === 'ONLINE'">Connected to: {{ server }}</span>
+      <span v-else>Disconnected from: {{ server }}</span>
     </div>
     <p> <img height="20" width="20" src="http://cultofthepartyparrot.com/parrots/hd/parrot.gif"/> received {{ count }} mqtt messages </p>
    </div>
@@ -16,7 +16,15 @@
         'mqtt_server',
         'mqtt_status',
         'count'
-      ])
+      ]),
+      server() {
+        const server = this.mqtt_server
+        if (server.indexOf('@') > 0) {
+          const scheme = server.split('://')[0]
+          const leftovers = server.split('@')[1]
+          return `${scheme}://[user]:[password]@${leftovers}`
+        }
+      }
     }
   }
 </script>
